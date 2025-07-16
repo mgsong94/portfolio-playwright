582377e5174abe532f4724ed5104f8c8dfa2d259
tysong94 <tysong0904@gmail.com>
2025-07-16 12:59:30 +0900

    Added Parameterize Tests

--- tests/upload.spec.ts
+++ tests/upload.spec.ts
@@ -5,29 +5,32 @@ const path = require('path')
 test.describe('Upload file', () => {
   let cartPage: CartPage
 
-  test('should upload a test file', async ({ page }) => {
-    cartPage = new CartPage(page)
+  const fileName = ['logotitle.png', '3mb-file.pdf']
 
-    // open url
-    await page.goto('https://practice.sdetunicorns.com/cart/')
-
-    // provide test file path
-    const filePath = path.join(__dirname, '../data/logotitle.png')
-    
-    // upload test file
-   cartPage.uploadComponent().uploadFile(filePath)
-
-    // hardcoded sleep = WRONG WAY(Hardcoded wait)
-    //  await page.waitForTimeout(5000)
-
-    // wait for condition(Conditional wait)
-    // await cartPage.uploadComponent().successTxt.waitFor({ state: 'visible', timeout: 10000})
-    
-    // Assertion wait
-    // assertion
-    await expect(cartPage.uploadComponent().successTxt).toContainText('uploaded successfully', {timeout: 10000})
-  })
+  for (const name of fileName) {
+    test(`should upload a ${name} file`, async ({ page }) => {
+      cartPage = new CartPage(page)
   
+      // open url
+      await page.goto('https://practice.sdetunicorns.com/cart/')
+  
+      // provide test file path
+      const filePath = path.join(__dirname, `../data/${name}`)
+      
+      // upload test file
+     cartPage.uploadComponent().uploadFile(filePath)
+  
+      // hardcoded sleep = WRONG WAY(Hardcoded wait)
+      //  await page.waitForTimeout(5000)
+  
+      // wait for condition(Conditional wait)
+      // await cartPage.uploadComponent().successTxt.waitFor({ state: 'visible', timeout: 10000})
+      
+      // Assertion wait
+      // assertion
+      await expect(cartPage.uploadComponent().successTxt).toContainText('uploaded successfully', {timeout: 10000})
+    })
+  }
 })
 
 
