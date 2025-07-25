82d3d84c4a6ab90bbb7561be28aa6006ba78da9f
tysong94 <tysong0904@gmail.com>
2025-07-25 09:28:16 +0900

    home page 보완

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -10,10 +10,15 @@ class HomePage {
   
   constructor(page: Page) {
     this.page = page;
-    this.getStartedBtn = page.locator('#get-started')
-    this.headingText = page.locator('text=think different. Make different.')
-    this.homeLink = page.locator('#zak-primary-menu').getByRole('link', { name: 'Home' })
+    // css selector
+    this.getStartedBtn = page.locator('#get-started') 
+    // text selector (""일 경우 대소문자 인식)
+    this.headingText = page.locator('text=think different. Make different.') 
+    // text and css selector
+    this.homeLink = page.locator('#zak-primary-menu:has-text("Home")') // this.homeLink = page.locator('#zak-primary-menu >> text=Home') 
+    // xpath selector
     this.searchIcon = page.locator('//div[@class="zak-header-actions zak-header-actions--desktop"]//a[@class="zak-header-search__toggle"]')
+    // multiple elements
     this.navLinks = page.locator('#zak-primary-menu li[id*=menu]')
   }
   
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -23,45 +23,47 @@ test.describe('Home', () => {
     await expect(page).toHaveTitle('About – Practice E-Commerce Site')
   })
   
-
   test('Click get started button using css selector', async ({ page }) => {
     // verify url does not have #get-started
-    await expect(page).not.toHaveURL(/.*#get-started/)
+    await expect(page).not.toHaveURL(/.*#get-started/) //regex
 
     // click the button
     await homePage.getStartedBtn.click()
 
     // verify url has #get-started
-    await expect(page).toHaveURL(/.*#get-started/)
+    await expect(page).toHaveURL(/.*#get-started/) //regex
   })
   
   test('Verify heading text is visible using text selector', async ({ page }) => {
-    // find the text locator
-    const headingText = homePage.headingText
+    // find the heading text
+    const headingText = homePage.headingText;
 
     // verify heading text is visible
-    await expect(headingText).not.toBeHidden()
-    await expect(headingText).toBeVisible()
+    await expect(headingText).not.toBeHidden();
+    await expect(headingText).toBeVisible();
   })
 
-
   test('Verify home link is enabled using text and css selector', async ({ page }) => {
-    // find the home text
-    const homeText = await homePage.homeLink
+    // find the home link
+    const homeLink = homePage.homeLink;
     
-    // verify url has #get-started
-    await expect(homeText).toBeEnabled()
+    // verify home link is enabled
+    await expect(homeLink).toBeEnabled();
   })
 
   test('Verify search icon is visible using xpath selector', async ({ page }) => {
     // find the search icon
-    const searchIcon = homePage.searchIcon
+    const searchIcon = homePage.searchIcon;
 
-    // verify url has #get-started
-    await expect(searchIcon).toBeVisible()
+    // verify search icon is visible
+    await expect(searchIcon).toBeVisible();
   });
 
-  test('Verify text of all nav links', async ({ page }) => {
+  test('Verify all texts of nav links', async ({ page }) => {
+    // find the nav links
+    const navLinks = homePage.navLinks;
+
+    // expected nav links
     const expectedLinks = [
       "Home",
       "About",
@@ -70,14 +72,16 @@ test.describe('Home', () => {
       "Contact",
       "My account"
     ]
-    // find the nav links
-    const navLinks = homePage.navLinks
 
     // verify nav links text
-    expect(await navLinks.allTextContents()).toEqual(expectedLinks)
+    expect(await navLinks.allTextContents()).toEqual(expectedLinks);
   })
 
-  test('Verify text of nav links', async ({ page }) => {
+  test('Verify nth text of nav links', async ({ page }) => {
+    // find the nav links
+    const navLink = homePage.navLinks.nth(3);
+    
+    // expected nav links
     const expectedLinks = [
       "Home",
       "About",
@@ -87,20 +91,17 @@ test.describe('Home', () => {
       "My account"
     ]
 
-    // find the nav links
-    const navLink = homePage.navLinks.nth(3)
-
     // verify nav links text
-    expect(await navLink.textContent()).toEqual(expectedLinks[3])
+    expect(await navLink.textContent()).toEqual(expectedLinks[3]);
   })
 
   test('Print out all the links', async ({ page }) => {
     // find the nav links
-    const navLinks = homePage.navLinks
+    const navLinks = homePage.navLinks;
     
     // print out all the links
     for (const el of await navLinks.elementHandles()) {
-      console.log(await el.textContent())
+      console.log(await el.textContent());
     }
   })
 })
