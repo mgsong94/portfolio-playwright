e2a56a9e060bb4fd4b81039e57dd021898b1b06f
tysong94 <tysong0904@gmail.com>
2025-07-30 15:45:12 +0900

    섹션 7: Wait Commands  보완

--- tests/4.upload.spec.ts
+++ tests/4.upload.spec.ts
@@ -54,8 +54,22 @@ test.describe('Upload file', () => {
     // upload test file
     cartPage.uploadComponent().uploadFile(filePath);
 
-    // assertion
-    await expect(cartPage.uploadComponent().successTxt).toContainText('uploaded successfully', { timeout: 10000 })
+    /*****************/
+    /* wait commands */
+    /*****************/
+    // Hardcoded wait - WRONG WAY
+    // 무조건 해당 타임아웃 시간이 끝날 때까지 기다림
+    // await page.waitForTimeout(5000);
+
+    // Conditional wait
+    // 요소가 조건을 만족시키면 타임아웃 시간과 상관없이 요소 반환. 최대 대기 시간의 개념
+    await page.locator('#wfu_messageblock_header_1_1').waitFor({state: 'visible', timeout: 10000});
+
+    // Assertion wait
+    // expect의 기본적인 타임아웃은 5초(설정에 있음)
+    // expect의 모든 함수는 기본적으로 timeout 매개변수를 가지고 있으므로 이를 추가하여 사용 가능
+    await expect(cartPage.uploadComponent().successTxt)
+      .toContainText('uploaded successfully', {timeout: 10000});
   })
 })
 
